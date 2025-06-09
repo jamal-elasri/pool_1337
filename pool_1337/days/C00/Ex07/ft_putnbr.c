e47c2901969dbfda3d77f04c2267191a2c880349
jamal el asri <153444893+jamal-elasri@users.noreply.github.com>
2025-06-09 07:48:18 +0100

    Add files via upload

---
+++ pool_1337/days/C00/Ex07/ft_putnbr.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void ft_put_char(char c){
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb){
+	if (nb == -2147483648){
+		ft_put_char('-');
+		ft_put_char(2);
+		ft_putnbr(147483648);
+		
+	}
+	else if(nb < 0){
+		ft_put_char('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if(nb > 9){
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else{
+	        ft_put_char(nb + 48);
+	}
+}
+
+

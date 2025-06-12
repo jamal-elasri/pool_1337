8bca21d7e35635e4b81cb66fee19a50da26bc51a
jamal el asri <153444893+jamal-elasri@users.noreply.github.com>
2025-06-12 07:09:59 +0100

    Add files via upload

---
+++ pool_1337/days/C02/Ex04/ft_str_is_lowercase.c
@@ -0,0 +1,15 @@
+int ft_str_is_lowercase(char *str){
+    int i = 0;
+    if (str[i] == '\0'){
+        return 1;
+    }
+    while(str[i] != '\0'){
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            i++;
+        }else {
+            return 0;
+        }
+    }
+    return 1;
+}

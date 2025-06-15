ed4e989817947bb621379b478d16150f7df74b4d
jamal el asri <153444893+jamal-elasri@users.noreply.github.com>
2025-06-15 09:53:45 +0100

    Add files via upload

---
+++ pool_1337/days/C03/Ex01/ft_strncmp.c
@@ -0,0 +1,12 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n){
+    int i = 0;
+
+    while ((s1[i] && s2[i]) && i < n){
+        if (s1[i] != s2[i]){
+            return (s1[i] - s2[i]);
+        }
+        i++;
+    }
+    return 0;
+}
+

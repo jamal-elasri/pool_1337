b6879f8b0057cb104468b0158beb2493c3c309fb
jamal el asri <153444893+jamal-elasri@users.noreply.github.com>
2025-06-16 06:43:11 +0100

    Update ft_putnbr.c

--- pool_1337/days/C00/Ex07/ft_putnbr.c
+++ pool_1337/days/C00/Ex07/ft_putnbr.c
@@ -1,26 +1,32 @@
-void ft_put_char(char c){
-	write(1, &c, 1);
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
 }
 
-void ft_putnbr(int nb){
-	if (nb == -2147483648){
-		ft_put_char('-');
-		ft_put_char('2');
-		ft_putnbr(147483648);
-		
-	}
-	else if(nb < 0){
-		ft_put_char('-');
-		nb = -nb;
-		ft_putnbr(nb + 48);
-	}
-	else if(nb > 9){
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else{
-	        ft_put_char(nb + 48);
-	}
+void ft_putnbr(int nb)
+{
+    if (nb == -2147483648)
+    {
+        ft_putchar('-');
+        ft_putchar('2');
+        ft_putnbr(147483648);
+    }
+    else if (nb < 0)
+    {
+        ft_putchar('-');
+        nb *= -1;
+        ft_putnbr(nb);
+    }
+    else if (nb > 9)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+    {
+        ft_putchar(nb + '0');
+    }
 }
-
 
